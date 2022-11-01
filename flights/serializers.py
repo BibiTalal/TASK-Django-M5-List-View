d4919c20e9a3410e522f6fecf0cd1e2d80c4a2e9
BibiTalal <bibi.t_alfahad@hotmail.com>
2022-11-01 04:19:39 +0300

    Done With LIst View Task

---
+++ flights/serializers.py
@@ -0,0 +1,14 @@
+from dataclasses import field, fields
+from pyexpat import model
+from flights.models import Flight, Booking
+from rest_framework import serializers
+
+class FlightListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Flight
+        fields=["id","destination","time","price"]
+
+class BookingListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Booking
+        fields=["flight","date","id"]
--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,15 @@
+from flights.serializers import BookingListSerializer, FlightListSerializer
 from .models import Booking, Flight
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from django.utils import timezone
+
+class FlightListView(ListAPIView):
+    queryset=Flight.objects.all()
+    serializer_class=FlightListSerializer
+
+class UpcomingBookingListView(ListAPIView):
+    queryset=Booking.objects.all()
+    serializer_class=BookingListSerializer
+
+
